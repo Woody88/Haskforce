d952a9b191596eec19bce1cdd02ce12dfa775510
Woodson Delhia <woodsondelhia88@mail.com>
2018-03-04 23:32:32 -0500

    added some example on how to use the Haskforce login and sobject function...

--- src/Haskforce.hs
+++ src/Haskforce.hs
@@ -12,9 +12,10 @@ module Haskforce
 import Data.Text (Text)
 import Haskforce.Client
 
-
-login :: ClientRequest a => a -> SFBaseUrl -> SFApiNumber -> IO (SFClient)
+-- I.E: login tokenRequest salesforceUrl "v42.0"
+login :: ClientRequest a => a -> SFBaseUrl -> SFApiNumber -> IO SFClient
 login = requestAuthentication
 
+-- I.E: sobject sfclient "Account" "<SobjectId>" ["Id", "Name"]
 sobject ::  SFClient -> Text -> Text -> [Text] -> IO SObject
 sobject = getSObject
